71e41072d4deb12dcfcce552e6dca16ea216fc59
Ryan Tablada <ryan.tablada@gmail.com>
2017-09-11 21:41:06 -0500

    Transport Ember App

---
+++ ember-templates/app.js
@@ -0,0 +1,7 @@
+export default Ember.HTMLBars.template({
+  "id": "ZBabGORM",
+  "block": {"symbols":[],"statements":[[6,"h1"],[7],[0,"\\n  "],[6,"a"],[10,"class",[26,["home-link ",[25,"if",[[19,0,["active"]],"active"],null]]]],[7],[0,"\\n    Hello "],[1,[18,"meetup"],false],[0,"\\n  "],[8],[0,"\\n"],[8],[0,"\\n"]],"hasEval":false},
+  "meta": {
+    "moduleName": "ember-templates/templates/application.hbs"
+  }
+});
